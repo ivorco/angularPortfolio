2bbc2beb54b524312adb1a5f2deb875866bf89d5
Or <2694823+ivorco@users.noreply.github.com>
2020-05-30 21:00:08 +0300

    Added components and changed name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angularTest'),
+      dir: require('path').join(__dirname, './coverage/angularPortfolio'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { IntroComponent } from './intro/intro.component';
+import { EnvironmentSelectorComponent } from './environment-selector/environment-selector.component';
+import { PhotosComponent } from './photos/photos.component';
+import { AppsComponent } from './apps/apps.component';
+import { ProductsComponent } from './products/products.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    IntroComponent,
+    EnvironmentSelectorComponent,
+    PhotosComponent,
+    AppsComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
