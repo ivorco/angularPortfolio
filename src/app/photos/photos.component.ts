ee2482b0849f9f7aa373d98bdd1f5064e0d531ff
Or <2694823+ivorco@users.noreply.github.com>
2020-06-01 16:47:07 +0300

    Added some sample photos

--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -1,15 +1,23 @@
+// https://medium.com/javascript-in-plain-english/how-to-create-an-image-gallery-app-with-angular-903190015aec
+
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../photo.service';
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
-  styleUrls: ['./photos.component.css']
+  styleUrls: ['./photos.component.css'],
 })
 export class PhotosComponent implements OnInit {
+  constructor(private photoService: PhotoService) {}
 
-  constructor() { }
+  photoUrls: string[] = [];
 
   ngOnInit(): void {
+    this.getPhotos();
   }
 
+  getPhotos() {
+    this.photoUrls = this.photoService.getPhotos();
+  }
 }
