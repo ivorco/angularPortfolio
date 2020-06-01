4efc13ec51e3db3d9359a23a46fa08004e62c25a
Or <2694823+ivorco@users.noreply.github.com>
2020-06-01 14:55:54 +0300

    Removed unused mat menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule, MatMenu } from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
