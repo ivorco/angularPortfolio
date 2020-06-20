a7ba806a146ec7de08c100803c2b9698ac9c62dd
Or <2694823+ivorco@users.noreply.github.com>
2020-06-20 14:08:28 +0300

    Photos component testing with stub works

--- src/app/photos/photos.component.spec.ts
+++ src/app/photos/photos.component.spec.ts
@@ -1,20 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotosComponent } from './photos.component';
+import { PhotoService, Photo } from '../photo.service';
 
 describe('PhotosComponent', () => {
   let component: PhotosComponent;
   let fixture: ComponentFixture<PhotosComponent>;
+  let photoServiceStub: Partial<PhotoService>;
+  // let photoService;
 
   beforeEach(async(() => {
+    photoServiceStub = {
+      async getPhotos(): Promise<Photo[]> {
+        return [
+          {
+            public_id: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Msn_logo.jpg',
+            version: 0,
+            format: '',
+            width: 100,
+            height: 100,
+            type: '',
+            created_at: '',
+          },
+        ];
+      },
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ PhotosComponent ]
-    })
-    .compileComponents();
+      declarations: [PhotosComponent],
+      providers: [{ provide: PhotoService, useValue: photoServiceStub }],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PhotosComponent);
+    // photoService = fixture.debugElement.injector.get(PhotoService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
